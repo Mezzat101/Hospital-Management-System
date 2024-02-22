54b6d9e8bc600f3c9dae0268e961ff6fdddb10c4
Your Name <mohamsdezzat@gmail.com>
2024-02-22 03:29:46 +0200

    Updating on the  Project and Building by CMAKE

---
+++ build/CMakeFiles/hospital_management.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hospital_management.dir/Hospital.cpp.o"
+  "CMakeFiles/hospital_management.dir/main.cpp.o"
+  "hospital_management"
+  "hospital_management.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hospital_management.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,164 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <ios>    //used to get stream size
+#include <limits> //used to get numeric limits
+#include "Hospital.hpp"
+
+using namespace std;
+
+int main()
+{
+    fstream f;
+    f.open("./data/appointments.csv", ios::in);
+    string temp, s, header;
+    getline(f, header);
+    getline(f, temp);
+    f.close();
+    stringstream str(temp);
+    getline(str, s, ',');
+    getline(str, s, ',');
+    int dd, mm, yyyy;
+    if (s != "")
+        cout << "\n\n\nLast usage date (DD-MM-YYYY) : " << s.substr(6, 2) + "-" + s.substr(4, 2) + "-" + s.substr(0, 4) + "\n";
+    cout << "\nPlease Enter Today's Date (DD-MM-YYYY) :\n\nEnter Day: ";
+    cin >> dd;
+    while (dd < 1 || dd > 31)
+    {
+        cout << "Invalid Day! Please enter a valid day: ";
+        cin >> dd;
+    }
+    cout << "Enter Month: ";
+    cin >> mm;
+    while (mm < 1 || mm > 12)
+    {
+        cout << "Invalid Month! Please enter a valid month: ";
+        cin >> mm;
+    }
+    cout << "Enter Year (YYYY): ";
+    cin >> yyyy;
+    yyyymmdd = yyyy * 10000 + mm * 100 + dd;
+    if (stoi(((s == "") ? ("0") : (s))) < yyyymmdd)
+    {
+        f.open("./data/temp.csv", ios::out);
+        f << header << "\n";
+        f.close();
+        remove("./data/appointments.csv");
+        rename("./data/temp.csv", "./data/appointments.csv");
+        fstream fout("./data/temp.csv", ios::out);
+        f.open("./data/doctors.csv", ios::in);
+        getline(f, temp);
+        fout << temp << endl;
+        while (getline(f, temp))
+        {
+            if (temp.size())
+                temp[temp.size() - 1] = '0';
+            fout << temp << endl;
+        }
+        f.close();
+        fout.close();
+        remove("./data/doctors.csv");
+        rename("./data/temp.csv", "./data/doctors.csv");
+    }
+    else if (stoi(s) > yyyymmdd && s != "")
+    {
+        cout << "\nEntered date detected wrong!\nToday's date can't be older than the last usage date, which is : "
+             << s.substr(6, 2) + "-" + s.substr(4, 2) + "-" + s.substr(0, 4) + "\n";
+        return 0;
+    }
+    // filling maps with data from csv files;
+    {
+        doctor d1;
+        patient p;
+        nurse n;
+        driver d2;
+        ambulance a1;
+        appointment a2;
+        d1.fillMap();
+        p.fillMap();
+        n.fillMap();
+        d2.fillMap();
+        a1.fillMap();
+        a2.fillMap();
+        // NOTE:
+        // fill drivers' Map before ambulances' Map;
+        // fill doctors' and patients' Map before appointments' Map;
+    }
+    while (1)
+    {
+        int category = 0;
+        cout << "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+        cout << "\nSelect a category:\n\n";
+
+        cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+        cout << "./HOME\n";
+        cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+        cout << "[01] : APPOINTMENTS\n";
+        cout << "[02] : PATIENTS\n";
+        cout << "[03] : DOCTORS\n";
+        cout << "[04] : NURSES\n";
+        cout << "[05] : DRIVERS\n";
+        cout << "[06] : AMBULANCES\n\n";
+        cout << "[-1] : EXIT\n";
+        cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n";
+        cout << "Enter your choice: ";
+        cin >> category;
+        cout << "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+        cout << "\n";
+        if (category == -1)
+        {
+            cout << "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+            cout << "\nShutting Down System...\n";
+            cout << "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+            break;
+        }
+        else if (category == 1)
+        {
+            appointmentsMenu();
+        }
+        else if (category == 2)
+        {
+            patientsMenu();
+        }
+        else if (category == 3)
+        {
+            doctorsMenu();
+        }
+        else if (category == 4)
+        {
+            nursesMenu();
+        }
+        else if (category == 5)
+        {
+            driversMenu();
+        }
+        else if (category == 6)
+        {
+            ambulancesMenu();
+        }
+        else
+        {
+            cout << "\nInvalid Choice!\n";
+        }
+
+        cout << endl;
+    }
+    // saving data inside maps by overwriting it on the files
+    {
+        doctor d1;
+        patient p;
+        nurse n;
+        driver d2;
+        ambulance a1;
+        appointment a2;
+        d1.saveMap();
+        p.saveMap();
+        n.saveMap();
+        d2.saveMap();
+        a1.saveMap();
+        a2.saveMap();
+    }
+
+    return 0;
+}
